b4c2183670b86d263576a4668ff91817e08fee36
CamiloQuea <camiloquea98@gmail.com>
2021-11-25 14:17:28 -0500

    V21

--- components/Dashboard/Productos/ListProductos.js
+++ components/Dashboard/Productos/ListProductos.js
@@ -139,128 +139,128 @@ export default function ListProductos({ shop_id }) {
     return (<>
 
 
-        <div className="">
 
-            {shop_id ? <div className='grid grid-cols-2'>
-                <div><button onClick={viewModal} className="p-4"> + Añadir</button></div>
-                <div className='p-4 justify-self-end'> <GlobalFilter preGlobalFilteredRows={preGlobalFilteredRows}
-                    setGlobalFilter={setGlobalFilter}
-                    globalFilter={state.globalFilter}
-                /></div>
-            </div> : <div className='grid grid-cols-1'> <div className='p-4 justify-self-start'> <GlobalFilter preGlobalFilteredRows={preGlobalFilteredRows}
+
+        {shop_id ? <div className='grid grid-cols-2'>
+            <div><button onClick={viewModal} className="p-4"> + Añadir</button></div>
+            <div className='p-4 justify-self-end'> <GlobalFilter preGlobalFilteredRows={preGlobalFilteredRows}
                 setGlobalFilter={setGlobalFilter}
                 globalFilter={state.globalFilter}
-            /></div></div>}
-            {modal == "" ? (<></>) : (<NewProducto shop_id={shop_id} handleModal={handleModal} />)}
+            /></div>
+        </div> : <div className='grid grid-cols-1'> <div className='p-4 justify-self-start'> <GlobalFilter preGlobalFilteredRows={preGlobalFilteredRows}
+            setGlobalFilter={setGlobalFilter}
+            globalFilter={state.globalFilter}
+        /></div></div>}
+        {modal == "" ? (<></>) : (<NewProducto shop_id={shop_id} handleModal={handleModal} />)}
 
 
-            <table {...getTableProps()} className="items-center bg-transparent w-full border-collapse text-center">
-                <thead>
-                    {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th
-                                    {...column.getHeaderProps(column.getSortByToggleProps())}
-                                    className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold"
-                                >
-                                    {column.render('Header')}
-                                    <span>
-                                        {column.isSorted
-                                            ? column.isSortedDesc
-                                                ? "▼"
-                                                : '▲'
-                                            : ''}
-                                    </span>
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {
-                        page.length > 0 ? (
-                            page.map((row, i) => {
-                                prepareRow(row)
-                                return (
-                                    <tr {...row.getRowProps()}>
-                                        {row.cells.map(cell => {
-                                            return (
-                                                <td
-                                                    {...cell.getCellProps()}
-                                                    className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-blueGray-700 "
-                                                >
-                                                    {cell.render('Cell')}
-                                                </td>
-                                            )
-                                        })}
-                                    </tr>
-                                )
-                            })
-
-                        ) : (
-                            <div>loading...</div>
-                        )
-                    }
-                </tbody>
-            </table>
-
-            <div className="pagination grid grid-cols-3 px-2">
-                <div className="justify-self-start">
-                    <span>
-                        Ir a la pag.:{' '}
-                        <input
-                            type="number"
-                            defaultValue={pageIndex + 1}
-                            onChange={e => {
-                                const page = e.target.value ? Number(e.target.value) - 1 : 0
-                                gotoPage(page)
-                            }}
-                            style={{ width: '100px' }}
-                        />
-                    </span>{' '}
-                </div>
-
-                <div className="justify-self-center">
-                    <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-                        {'<<'}
-                    </button>{' '}
-                    <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-                        {'<'}
-                    </button>{' '}
-                    <button onClick={() => nextPage()} disabled={!canNextPage}>
-                        {'>'}
-                    </button>{' '}
-                    <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-                        {'>>'}
-                    </button>{' '}
-                    <span>
-                        Pagina{' '}
-                        <strong>
-                            {pageIndex + 1} of {pageOptions.length}
-                        </strong>{' '}
-                    </span>
-                </div>
-
-
-                <div className="justify-self-end">
-                    <select
-                        value={pageSize}
-                        onChange={e => {
-                            setPageSize(Number(e.target.value))
-                        }}
-                    >
-                        {[10, 20, 30, 40, 50].map(pageSize => (
-                            <option key={pageSize} value={pageSize}>
-                                Mostrar {pageSize}
-                            </option>
+        <table {...getTableProps()} className="items-center bg-transparent w-full border-collapse text-center">
+            <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th
+                                {...column.getHeaderProps(column.getSortByToggleProps())}
+                                className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold"
+                            >
+                                {column.render('Header')}
+                                <span>
+                                    {column.isSorted
+                                        ? column.isSortedDesc
+                                            ? "▼"
+                                            : '▲'
+                                        : ''}
+                                </span>
+                            </th>
                         ))}
-                    </select>
-                </div>
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
 
+                {
+                    page.length > 0 ? (
+                        page.map((row, i) => {
+                            prepareRow(row)
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map(cell => {
+                                        return (
+                                            <td
+                                                {...cell.getCellProps()}
+                                                className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-blueGray-700 "
+                                            >
+                                                {cell.render('Cell')}
+                                            </td>
+                                        )
+                                    })}
+                                </tr>
+                            )
+                        })
 
+                    ) : (
+                        <div>loading...</div>
+                    )
+                }
 
+            </tbody>
+        </table>
+
+        <div className="pagination grid grid-cols-3 px-2 mt-auto">
+            <div className="justify-self-start">
+                <span>
+                    Ir a la pag.:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                        }}
+                        style={{ width: '100px' }}
+                    />
+                </span>{' '}
             </div>
-            {/* <table className="items-center bg-transparent w-full border-collapse text-center">
+
+            <div className="justify-self-center">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Pagina{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+            </div>
+
+
+            <div className="justify-self-end">
+                <select
+                    value={pageSize}
+                    onChange={e => {
+                        setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[10, 20, 30, 40, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Mostrar {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+        </div>
+        {/* <table className="items-center bg-transparent w-full border-collapse text-center">
                 <thead>
                     <tr >
                         <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold">
@@ -347,7 +347,7 @@ export default function ListProductos({ shop_id }) {
             </table> */}
 
 
-        </div>
+
 
     </>)
 }
