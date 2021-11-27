b39e169cf2f8962c3e116dd9ce336dbe089a584b
CamiloQuea <camiloquea98@gmail.com>
2021-11-27 12:56:42 -0500

    final frontend

--- components/Dashboard/Configuracion/addImage.js
+++ components/Dashboard/Configuracion/addImage.js
@@ -49,12 +49,15 @@ export default function addImage(props) {
             <div className="bg-white dark:bg-pruebaA3 rounded-3xl p-10 md:w-3/4 lg:w-1/2 mx-auto">
                 <h2 className="dark:text-white font-bold text-gray-600 ">Modificar imagen</h2>
 
-                <form onSubmit={handleSubmit} method="PATCH" action="https://api.abako.xyz/api/user">
+                <form onSubmit={handleSubmit} method="PATCH" action="https://api.abako.xyz/api/user" className="flex flex-col">
 
                     <input type="file" name="user_image" onChange={handleChange} className="md:text-left text-center flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 
                               text-gray-600 dark:text-white outline-none dark:bg-pruebaA3"/>
-                    <button className="bg-gray-900 text-white p-3" type="submit" >Agregar</button>
-                    <button className="py-3 px-8 bg-red-400 text-white font-bold" onClick={handleModal} >Cerrar</button>
+                    <div>
+                        <button className="bg-gray-900 text-white p-3" type="submit" >Agregar</button>
+                        <button className="py-3 px-8 bg-red-400 text-white font-bold" onClick={handleModal} >Cerrar</button>
+
+                    </div>
 
                 </form>
 
