486dff6549cc4191c51dd4227fff696c326966b7
CamiloQuea <camiloquea98@gmail.com>
2021-11-27 12:57:56 -0500

    final frontend

--- components/Dashboard/Configuracion/addImage.js
+++ components/Dashboard/Configuracion/addImage.js
@@ -53,7 +53,7 @@ export default function addImage(props) {
 
                     <input type="file" name="user_image" onChange={handleChange} className="md:text-left text-center flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 
                               text-gray-600 dark:text-white outline-none dark:bg-pruebaA3"/>
-                    <div>
+                    <div className="mt-3">
                         <button className="bg-gray-900 text-white p-3" type="submit" >Agregar</button>
                         <button className="py-3 px-8 bg-red-400 text-white font-bold" onClick={handleModal} >Cerrar</button>
 
