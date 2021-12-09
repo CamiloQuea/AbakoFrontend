39dca085831524503568b493e0223c0bcc55b8ba
CamiloQuea <camiloquea98@gmail.com>
2021-12-09 17:54:47 -0500

    final

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -43,7 +43,7 @@ export default function inicio() {
 
 
                         <div className="p-2 inline-flex w-full">
-                            <div className="w-14 h-14 bg-white relative -my-8 ml-5 mr-3 rounded-xl shadow-lg grid grid-cols-1 dark:bg-pruebaA3">
+                            <div className="w-14 h-14 bg-white relative -my-8 ml-5 mr-3 rounded-xl shadow-lg grid grid-cols-1 dark:bg-pruebaA3 z-0">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-red-500 justify-self-center self-center" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z" clipRule="evenodd" />
                                 </svg>
@@ -99,7 +99,7 @@ export default function inicio() {
 
                     </div>
 
-                    <div className='rounded-xl lg:row-span-2 lg:col-span-1 lg:row-start-1 lg:col-start-5  shadow-md h-full border dark:border-prueba flex flex-col'>
+                    <div className='rounded-xl lg:row-span-2 lg:col-span-1 lg:row-start-1 lg:col-start-5  shadow-md h-full border dark:border-prueba flex flex-col z-0'>
 
 
 
