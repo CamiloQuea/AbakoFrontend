11729d90e2cf4a8337f3f2ef024be08156dda9e6
CamiloQuea <camiloquea98@gmail.com>
2021-12-08 19:31:50 -0500

    final

--- pages/dashboard/configuracion.js
+++ pages/dashboard/configuracion.js
@@ -48,7 +48,7 @@ export default function configuracion() {
 
             <div className="grid xl:grid-cols-2 xl:gap-4 gap-10 grid-cols-1 mx-8 mt-6 xl:mb-0 mb-20">
                 <div className="xl:col-span-2 justify-center items-center justify-self-center mb-8">
-                    <div className="justify-center flex">
+                    <div className="h-80 w-80 flex mx-auto">
 
                         {/* <div className="rounded-full bg-gray-900 mt-7">
 
@@ -59,8 +59,8 @@ export default function configuracion() {
 
                         </div> */}
 
-                        <img className="object-cover h-80 w-80 rounded-full" src={data.user_image ? data.user_image : '/user.png'} />
-                        <div className="self-end cursor-pointer">
+                        <img className="shadow-xl object-cover rounded-full border border-gray-900 subpixel-antialiased " width="300" height="300" src={data.user_image ? data.user_image : '/user.png'} />
+                        <div className="cursor-pointer min-w-0 self-end">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={viewModal}>
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
