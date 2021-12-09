7708715cb08848fc31a79b5cc0a7dcec1247b5ac
CamiloQuea <camiloquea98@gmail.com>
2021-12-08 19:37:39 -0500

    final

--- pages/dashboard/configuracion.js
+++ pages/dashboard/configuracion.js
@@ -59,7 +59,7 @@ export default function configuracion() {
 
                         </div> */}
 
-                        <img className="shadow-xl object-cover rounded-full" width="300" height="300" src={data.user_image ? data.user_image : '/user.png'} />
+                        <img className="object-cover rounded-full" width="300" height="300" src={data.user_image ? data.user_image : '/user.png'} />
                         <div className="cursor-pointer min-w-0 self-end">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={viewModal}>
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
